0515e023b951100224d2b9b0ca9def48ca0ed818
breannams <breannamarie_s@hotmail.com>
2021-07-09 01:20:37 -0400

    commit tree 723e7f5b399c4dd183a9b77cf2cb0bc27879f0dd

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
 /* Your Code Here */
+function createEmployeeRecord(arr){
+    let employeeRecord = {   
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [] ,
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+
+let createEmployeeRecords =  function(record){
+    return record.map(createEmployeeRecord)
+}
+// function createEmployeeRecords(arr){
+//     return  arr.map(createEmployeeRecord);
+// }
+
+let createTimeInEvent = function(dateStamp){
+        const newTimeIn = {
+            type: "TimeIn",
+            hour: parseInt(dateStamp.slice(-4)),
+            date: dateStamp.slice(0, 10)
+        }
+        this.timeInEvents.push(newTimeIn)
+        return this
+    }
+// function createTimeInEvent(rec, dateStamp){
+//     const newTimeIn = {
+//         type: "TimeIn",
+//         hour: parseInt(dateStamp.slice(-4)),
+//         date: dateStamp.slice(0, 10)
+//     }
+
+//     rec.timeInEvents.push(newTimeIn)
+//     return rec
+// }
+
+
+let createTimeOutEvent = function(dateStamp){
+    const newTimeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeOutEvents.push(newTimeOut)
+    return this
+}
+// function createTimeOutEvent(rec, dateStamp){
+//     const newTimeOut = {
+//         type: "TimeOut",
+//         hour: parseInt(dateStamp.slice(-4)),
+//         date: dateStamp.slice(0,10)
+//     }
+//     rec.timeOutEvents.push(newTimeOut)
+//     return rec
+// }
+
+
+let hoursWorkedOnDate = function(date){
+    const timeIn = this.timeInEvents.find(e => e.date === date).hour
+    const timeOut  = this.timeOutEvents.find( e => e.date === date).hour
+    return (timeOut - timeIn)/100;
+}
+// function hoursWorkedOnDate(rec, date){
+//     const timeIn = rec.timeInEvents.find(e => e.date === date).hour
+//     const timeOut  = rec.timeOutEvents.find( e => e.date === date).hour
+//     return (timeOut - timeIn)/100;
+// }
+
+let wagesEarnedOnDate = function(date){
+    const pay = this.payPerHour
+    return pay * hoursWorkedOnDate.call(this, date)
+}
+// function wagesEarnedOnDate(rec, date){
+//  const pay =  rec.payPerHour
+//  return pay * hoursWorkedOnDate(rec, date)
+// }
+
+
+
+function findEmployeeByFirstName(empNames, firstName){
+    return empNames.find(employee => employee.firstName == firstName)
+}
+
+
+let calculatePayroll = function(rec){
+   return rec.reduce(function(total, wages){
+        return total + allWagesFor.call(wages)}, 0)
+}
+
+// function calculatePayroll(rec){
+//     const employeeTotals = rec.map(employees => allWagesFor(employees))
+//     return employeeTotals.reduce((total, wages) => total + wages)
+// }
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
